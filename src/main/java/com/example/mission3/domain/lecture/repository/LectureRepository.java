84818fed4d88927d5ca6db430cbbb6e266fb8ffd
openmpy <suhwan@kakao.com>
2024-02-29 01:11:58 +0900

    feat: 카테고리별 강의 목록 조회 기능

--- src/main/java/com/example/mission3/domain/lecture/repository/LectureRepository.java
+++ src/main/java/com/example/mission3/domain/lecture/repository/LectureRepository.java
@@ -1,6 +1,7 @@
 package com.example.mission3.domain.lecture.repository;
 
 import com.example.mission3.domain.lecture.entity.Lecture;
+import com.example.mission3.domain.lecture.entity.type.CategoryType;
 import com.example.mission3.domain.teacher.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,6 @@ import java.util.List;
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
 
     List<Lecture> findAllByTeacherOrderByCreatedAtDesc(Teacher teacher);
+
+    List<Lecture> findAllByCategoryOrderByCreatedAtDesc(CategoryType category);
 }
